ad0182e5e199c7e509b2a9b1145b707c5e6980c8
George Ivanov <george.ivanov@kcl.ac.uk>
2022-10-21 17:50:09 +0100

    add functionality to frequencies

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,11 @@
 
 def frequencies(items):
     frequencies = {}
+    for item in items:
+        key = str(item)
+        if (key in frequencies.keys()):
+            frequencies[key] = frequencies[key] + 1
+        else:
+            frequencies.update({key: 1})
     # Your code goes here
     return frequencies
